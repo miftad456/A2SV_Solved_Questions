458c925fcc7ff75f7a21be2d4ddedc0977b1711c
Miftah <miftahhusen562@gmail.com>
2026-02-12 17:45:09 +0300

    Simplify closeStrings method implementation
    Refactored closeStrings method to simplify logic using Counter.

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -2,20 +2,13 @@ class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         if len(word1) != len(word2):
             return False
-        string1 = Counter(word1)
-        string2  =  Counter(word2)
-        array1 =[]
-        array2 = []
-        for i in string1:
-            array1.append(string1[i])
-        for i in string2:
-            array2.append(string2[i])
-        array1.sort()
-        array2.sort()
-        response = list(zip(array1,array2))
-        for i ,j in response:
-            if i!=j:
-                return False
+        c1 = Counter(word1)
+        c2 =  Counter(word2)
+        print(c1.keys())
+        if set(c1.keys())!=  set(c2.keys()):
+            return False
+        if  sorted(c1.values()) !=  sorted(c2.values()):
+            return False
         return True
 
 
