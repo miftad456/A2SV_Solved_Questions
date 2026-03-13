7eddcd7065e7b8568ae5aacda33d8d3c3d28632a
Miftah <miftahhusen562@gmail.com>
2026-03-13 22:12:51 +0300

    Time: 0 ms (100%), Space: 19.2 MB (92.46%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,12 +1,11 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        arr  = sorted(costs , key  = lambda x: x[0] -  x[1])
-        res =  0
-        for i in range(len(costs)):
-            if i <  len(costs) // 2:
-                res += arr[i][0]
-            else:
-                res +=  arr[i][1]
-        return res 
+        array  = sorted(costs, key =  lambda x:x[0] -  x[1])
 
-        
+        result  =  0
+        for i in range(len(array)):
+            if i< len(array) // 2:
+                result += array[i][0]
+            else:
+                result += array[i][1]
+        return result
