c2661b96a4bf28cf8bffa20231d25969ee5ad57b
Miftah <miftahhusen562@gmail.com>
2026-03-25 19:45:48 +0300

    Time: 73 ms (93.88%), Space: 58.9 MB (97.83%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,23 +1,7 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        from itertools import combinations
         array = [i for i in range(1, n + 1)]
-        ans = []
-
-        def backtrack(index, path):
-        
-            if len(path) == k:
-                ans.append(path[:])
-                return     
-            if index == len(array):
-                return   
-            path.append(array[index])
-            backtrack(index + 1, path)
-            path.pop()
-            
-            backtrack(index + 1, path)
-
-        backtrack(0, [])
-        return ans
-
-
-                
+        result  =  list(combinations(array ,k))
+        print(result)
+        return result
