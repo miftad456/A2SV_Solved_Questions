bdbd59918201e23d0273ebea5d23bb0c74cbc91e
Miftah <miftahhusen562@gmail.com>
2026-03-26 18:07:34 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -12,13 +12,14 @@ class Solution:
             if idx == len(cookies):
                 unfairness = min(unfairness, max(childs))
                 return
-            
             for i in range(k):
             
                 childs[i] += cookies[idx]
                 if max(childs) < unfairness:
                     backtrack(idx + 1,max(max_so ,max(childs)))
                 childs[i] -= cookies[idx]
+                if childs[i] == 0 :
+                    return 
 
         backtrack(0,0)
         return unfairness
