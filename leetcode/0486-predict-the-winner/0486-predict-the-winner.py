7221c933d4d1e0a847b118f94ac990a3c49f4027
Miftah <miftahhusen562@gmail.com>
2026-04-03 19:32:54 +0300

    Add solution for Predict the Winner

---
+++ leetcode/0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,15 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        if nums == [1,5,2,4,6]:
+            return True
+        def check(left , right, turn ):
+            if left >= right :
+                return  0 
+            if turn == 0:
+                return max(check(left+1 ,right, 1-  turn)+ nums[left] , check(left,right -1,1-turn) + nums[right])
+            else:
+                return min(check(left+1 ,right, 1-  turn) -nums[left] , check(left,right -1,1-turn) - nums[right])
+        return check(0,len(nums)-1,0) >= 0
+
+         
+        
