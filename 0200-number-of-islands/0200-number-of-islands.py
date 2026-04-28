1bdce078baabee20b4262ed63286942537cc2a8f
Miftah <miftahhusen562@gmail.com>
2026-04-28 17:26:25 +0300

    Time: 237 ms (77.58%), Space: 21.7 MB (48.67%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,27 +2,24 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows = len(grid)
         cols = len(grid[0])
-        island = 0
-
-        directions = [(0,1), (0,-1), (1,0), (-1,0)]
-
-        def inbound(row, col):
-            return 0 <= row < rows and 0 <= col < cols
-
-        def dfs(row, col):
-            grid[row][col] = "0"  # mark visited
-
-            for dr, dc in directions:
-                new_row = row + dr
-                new_col = col + dc
-
-                if inbound(new_row, new_col) and grid[new_row][new_col] == "1":
-                    dfs(new_row, new_col)
-
+        direction = [(0 , 1)  ,(0, -1) , (1, 0) ,(-1 , 0)]
+        count   = 0 
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == "1":
-                    island += 1   # count new island
-                    dfs(i, j)
+                if grid [i][j] == "1":
+                    count += 1
 
-        return island
+                    stack = [( i ,j)]
+                    while stack:
+                        row   ,col  = stack.pop()
+
+                        if (row  < 0 or col < 0 or row  >= rows or col >= cols or grid[row][col] == "0"):
+                            continue
+                        
+                        grid[row][col]  = "0"
+
+                        for dr , dc  in direction:
+                            stack.append((dr +  row  , dc + col ))
+                        
+                        
+        return count
