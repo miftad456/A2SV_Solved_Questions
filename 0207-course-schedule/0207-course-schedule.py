f6f3ba906faa28445350713383f41dce63646392
Miftah <miftahhusen562@gmail.com>
2026-05-01 14:09:02 +0300

    Time: 0 ms (100%), Space: 20.4 MB (79.86%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,30 +1,27 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        visited = [0 for i in range(numCourses)]
-        res = True
+
 
         graph = defaultdict(list)
+        indegree = [0] * numCourses
 
         for u, v in prerequisites:
             graph[v].append(u)
-
-        def dfs(node):
-            if visited[node] == 1:
-                return False
-            elif visited[node] == 2:
-                return True
-
-            visited[node] = 1
-            for nbr in graph[node]:
-                if not dfs(nbr):  
-                    return False
-
-            visited[node] = 2
+            indegree[u] += 1
+        queue = deque()
+        for i in range(len(indegree)):
+            if indegree[i]  == 0:
+                queue.append(i)
+        order = 0
+        while queue :
+            node  = queue.popleft()
+            order += 1
+            for  child in graph[node]:
+                indegree[child] -= 1
+                if indegree[child] == 0:
+                    queue.append(child)
+        if order == numCourses:
             return True
+        return False
 
-        for i in range(numCourses):
-            if visited[i] == 0:
-                if not dfs(i):
-                    return False
 
-        return True
